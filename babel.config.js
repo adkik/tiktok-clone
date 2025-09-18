035ad311c2236df2bab7e2bbc1a905a8fbf45875
adkik <adkikabidze@gmail.com>
2025-09-18 21:51:17 +0400

    fix: add babel config for unistyles

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    // for bare React Native
+    // presets: ['module:@react-native/babel-preset'],
+
+    // or for Expo
+    presets: ['babel-preset-expo'],
+
+    // other config
+    plugins: [
+        // other plugins
+        ['react-native-unistyles/plugin', {
+            root: 'src'
+        }], 
+        'react-native-reanimated/plugin',
+    ]
+  }
+}
