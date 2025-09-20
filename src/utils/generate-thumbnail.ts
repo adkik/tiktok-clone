90019d628026e74947f0a6f903d56c17edc95d34
adkik <adkikabidze@gmail.com>
2025-09-21 02:22:03 +0400

    fix: convert seconds to ms

--- src/utils/generate-thumbnail.ts
+++ src/utils/generate-thumbnail.ts
@@ -3,7 +3,7 @@ import * as VideoThumbnails from "expo-video-thumbnails";
  * Generates a thumbnail from a video
  *
  * @param url Video URL.
- * @param duration Video duration in milliseconds.
+ * @param duration Video duration in seconds.
  * @returns Promise of thumbnail URI, or null on failure.
  */
 export async function generateThumbnail(url: string, duration: number) {
@@ -14,7 +14,7 @@ export async function generateThumbnail(url: string, duration: number) {
 
   // 3s or duration - 0.5s, but never below 0
   const safeTime = duration
-    ? Math.max(Math.min(3000, duration - 500), 0)
+    ? Math.max(Math.min(3000, duration * 1000 - 500), 0)
     : 3000;
 
   try {
