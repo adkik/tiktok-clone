2d049e06d830ad12ca768c0004389779146d830f
adkik <adkikabidze@gmail.com>
2025-09-23 01:09:28 +0400

    test: mock Unistyles

---
+++ __mocks__/react-native-unistyles.ts
@@ -0,0 +1,14 @@
+export const StyleSheet = {
+  create: (styles: any) => styles,
+  configure: jest.fn(),
+};
+
+export const useUnistyles = () => {
+  // import at runtime to break circular dependency
+  const themeModule = require("@/styles/theme");
+
+  return {
+    theme: themeModule.lightTheme, // override per-test if needed
+    rt: { insets: { top: 0, bottom: 0, left: 0, right: 0 } },
+  };
+};
